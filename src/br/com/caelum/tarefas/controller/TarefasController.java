638246b0d8b27c1238e924be40401dfc125fe9f8
Tiago T Soares <tantiago@gmail.com>
2017-07-26 22:19:29 -0300

    Exercicios 11.14

--- src/br/com/caelum/tarefas/controller/TarefasController.java
+++ src/br/com/caelum/tarefas/controller/TarefasController.java
@@ -3,6 +3,7 @@ package br.com.caelum.tarefas.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -27,6 +28,13 @@ public class TarefasController {
 		JdbcTarefaDao dao = new JdbcTarefaDao();
 		dao.adiciona(tarefa);
 		return "tarefa/adicionada";
+	}
+	
+	@RequestMapping("listaTarefas")
+	public String lista(Model model) {
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		model.addAttribute("tarefas", dao.lista());
+		return "tarefa/lista";
 	}
 	
 }
